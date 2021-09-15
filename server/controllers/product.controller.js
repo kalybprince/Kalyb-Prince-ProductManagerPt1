b482d7c56794916c893204d5d40f9c708a60693e
Kalyb Prince <kalybprince91@gmail.com>
2021-09-14 16:39:43 -1000

    init server.js, App.js, Main.js

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,19 @@
+const { Product } = require("../models/Product.model");
+
+module.exports.index = (req, res) => {
+    res.json({
+       message: "Hello World"
+    });
+}
+
+module.exports.createProduct = (req, res) => {
+    const {title, price, description} = req.body;
+    
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(product => res.json(product))
+        .catch(err => res.json(err))
+}
